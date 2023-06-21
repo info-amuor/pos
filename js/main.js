c4225ad79144efbdc2e9612b274f1ef2b1dea3c2
ozen-hub <info.ozenhub@gmail.com>
2023-06-21 22:16:51 +0530

    6

--- js/main.js
+++ js/main.js
@@ -11,13 +11,12 @@ class User {
 
 /*======User Management*/
 const createUser=()=>{
-
     const fullName=$('#full-name').val();
     const email=$('#register-email').val();
     const password=$('#register-password').val();
 
-    const hash = bcrypt.hashSync(password,bcrypt.genSaltSync(10));
-    console.log(hash)
+    //const hash = bcrypt.hashSync(password,bcrypt.genSaltSync(10));
+    //console.log(hash)
     let createdUser=new User(fullName,email,password);
     let existsUserData=userArr.find(e=>e.email===email);
     if (existsUserData){
@@ -27,6 +26,25 @@ const createUser=()=>{
     userArr.push(createdUser);
     console.log(userArr);
 }
+
+const login=()=>{
+    const email=$('#email').val();
+    const password=$('#password').val();
+
+    let existsUserData=userArr.find(e=>e.email===email);
+
+    if (existsUserData){
+        if(existsUserData.password===password){
+            //login=> dashboard
+        }else{
+            alert('password is wrong');
+            return
+        }
+    }else{
+        alert('User email not found');
+    }
+}
+
 /**/
 
 const clearAndLoad=(element)=>{
