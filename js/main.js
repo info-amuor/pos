e2c18ab05ce43f04235668caa496e953485ba20b
ozen-hub <info.ozenhub@gmail.com>
2023-06-21 21:35:17 +0530

    3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
+let userArr=[];
+class User {
+    constructor(fullName,email,password) {
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+    }
+}
+
+
+
+/*======User Management*/
+const createUser=()=>{
+    let createdUser=new User('','','');
+    userArr.push(createdUser);
+}
+/**/
+
 const clearAndLoad=(element)=>{
-    let body = $('body').empty();
-    let tempElement=$('#'+element);
-    tempElement.appendTo(body);
+  // clear, toggle
 }
